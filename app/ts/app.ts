721d62a207201a245d129c58017372c90c542bd1
Telma Gama <telmagamasilva@gmail.com>
2018-03-13 22:10:18 -0300

    initial commit

---
+++ app/ts/app.ts
@@ -0,0 +1,6 @@
+const controller = new NegociacaoController();
+document
+        .querySelector('.form')
+        .addEventListener('submit', controller.adiciona.bind(controller))
+        //esse bind é para que o this tenha foco no objeto controller - meotdo submit
+        //sem ele o foco do this seria perdido para o proposito de submeter o formulario
